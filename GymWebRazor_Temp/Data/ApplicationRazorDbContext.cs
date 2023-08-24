b0a769f75facb1684fec1b96f140bd872975dfd9
OkejoTM <denastytiko03@mail.ru>
2023-08-24 13:55:41 +0300

    add razor pages project

---
+++ GymWebRazor_Temp/Data/ApplicationRazorDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GymWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymWebRazor_Temp.Data
+{
+    public class ApplicationRazorDbContext : DbContext  
+    {
+        public ApplicationRazorDbContext(DbContextOptions<ApplicationRazorDbContext> options) : base(options) { }       
+
+        DbSet<WorkoutPlan> WorkoutPlans { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WorkoutPlan>().HasData(
+                new WorkoutPlan { Id = 1, Date = new DateTime(2023, 8, 1), Notes = "Triceps" },
+                new WorkoutPlan { Id = 2, Date = new DateTime(2023, 8, 3), Notes = "Bicceps" },
+                new WorkoutPlan { Id = 3, Date = new DateTime(2023, 8, 5), Notes = "Chest" });
+        }
+    }
+}
---
+++ GymWebRazor_Temp/Models/WorkoutPlan.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace GymWebRazor_Temp.Models
+{
+    public class WorkoutPlan
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Chose Date and Time")]
+        [Required]
+        public DateTime Date { get; set; }
+
+        [DisplayName("Write Some Notes")]
+        [MaxLength(120, ErrorMessage = "Maximum length is 120 symbols!")]
+        public string Notes { get; set; }
+    }
+}
